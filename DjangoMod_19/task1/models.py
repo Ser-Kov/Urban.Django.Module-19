210df67c173913905fba6813da9044138cc21c1f
Cергей Ковалевский <ser.kovalevskiy@gmail.com>
2024-08-31 13:17:06 +0300

    Домашнее задание по теме "Модели баз данных в Django"

---
+++ DjangoMod_19/task1/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Buyer(models.Model):
+    name = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    balance = models.DecimalField(max_digits=5, decimal_places=2)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=30)
+    cost = models.DecimalField(max_digits=5, decimal_places=2)
+    size = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games')
+
